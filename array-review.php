1ecfb7591aa2469592d1bfe37cf54739db976c9f
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-01-15 00:58:21 -0500

    Initial commit

---
+++ array-review.php
@@ -0,0 +1,8 @@
+<?php
+    /*
+     * Kianna Dyck
+     * 01/14/2018
+     * array-review.php
+     * This script contains a series of exercises designed
+     * to review php arrays and functions.
+    */
